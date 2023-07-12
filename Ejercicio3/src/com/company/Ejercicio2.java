ddd27ec8a9522dfb02c29ae3d7785cd967f44200
Edwin E <edwin.estrada1204@gmail.com>
2023-07-12 11:08:50 -0600

    Ejercicio3

---
+++ Ejercicio3/src/com/company/Ejercicio2.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        String[] nombres={"nombre1","nombre2","nombre3"};
+
+        for(String nombre:nombres){
+            System.out.println(nombre);
+        }
+    }
+}
